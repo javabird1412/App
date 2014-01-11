b871ce294723a34865b6cc16aa87e774d4eff361
javabird1412 <javabird1412@users.noreply.github.com>
2014-01-11 21:25:04 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,23 @@ function sayhello() {
 	alert('Hallo Grossmeister!');
 }	
 
+(function(){
+  function install(ev) {
+    ev.preventDefault();
+    // define the manifest URL
+    var manifest_url = "http://javabird1412.github.io/App/manifest.webapp";
+    // install the app
+    var myapp = navigator.mozApps.install(manifest_url);
+    myapp.onsuccess = function(data) {
+      // App is installed, remove button
+      this.parentNode.removeChild(this);
+    };
+    myapp.onerror = function() {
+      // App wasn't installed, info is in this.error.name
+      console.log('Install failed, error: ' + this.error.name);
+     };
+  };
+  // get a reference to the button and call install() on click
+  var button = document.getElementById('install');
+  button.addEventListener('click', install, false);
+})();
